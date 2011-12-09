78183e616272ec99fd71b013e290a9088b86f6c7
Daniel Torreblanca <d.torreblanca@syncapse.com>
2011-12-09 15:49:12 -0500

    Vagrant updates (php config, vim, etc)

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -9,16 +9,30 @@
 
 include_recipe "apache2"
 include_recipe "apache2::mod_rewrite"
-include_recipe "apache2::mod_php5"
 
 web_app "remod" do
   server_name "remod.dev"
   docroot "/srv/http"
 end
 
-# include_recipe "mysql::mysql_client"
+include_recipe "apache2::mod_php5"
+
+file = File.new( "/var/log/php.log", "w" )
+file.chmod( 0644 )
+file.chown(33, 33)
+file.close
+
+template "/etc/php/php.ini" do
+  source "php.ini.erb"
+  notifies :restart, resources(:service => "apache2")
+end
+
 
 package "nodejs" do
+  action :install
+end
+
+package "vim" do
   action :install
 end
 

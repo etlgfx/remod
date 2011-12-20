b84f67c10d0a9f492f5168ab30e674a486ff70cd
Daniel Torreblanca <d.torreblanca@syncapse.com>
2011-12-20 15:39:29 -0500

    Refactor Config::read so that it's no longer stupid

--- src/classes/Config.class.php
+++ src/classes/Config.class.php
@@ -7,11 +7,12 @@ class Config {
     const KEY_NEST_LIMIT = 3;
 
     private function __construct() {
-        if (file_exists(PATH_CONFIG .'config.ini')) {
-            $this->config = parse_ini_file(PATH_CONFIG .'config.ini', true);
-        } else {
-            $this->config = parse_ini_file(PATH .'/config/config.ini', true);
-        }
+//        if (file_exists(PATH_CONFIG .'config.ini')) {
+//            $this->config = parse_ini_file(PATH_CONFIG .'config.ini', true);
+//        } else {
+//            $this->config = parse_ini_file(PATH .'/config/config.ini', true);
+//        }
+        $this->config = array();
     }
 
     protected static function getInstance()
@@ -24,36 +25,17 @@ class Config {
     }
 
     public static function read($key) {
-        $key = explode('.', $key, self::KEY_NEST_LIMIT);
-        $depth = count($key);
-
+        $config = explode('.', $key, self::KEY_NEST_LIMIT);
         $inst = self::getInstance();
+        $obj =& $inst->config;
 
-        switch($depth) {
-            case 1:
-                if ( !empty($inst->config[$key[0]]) ) {
-                    return $inst->config[$key[0]];
-                } else {
-                    throw new Exception('Missing configuration: ' . $key[0]);
-                }
-                break;
-
-            case 2:
-                if ( !empty($inst->config[$key[0]][$key[1]]) ) {
-                    return $inst->config[$key[0]][$key[1]];
-                } else {
-                    throw new Exception('Missing configuration: ' . $key[0] . '.' . $key[1]);
-                }
-                break;
-
-            case 3:
-                if ( !empty($inst->config[$key[0]][$key[1]][$key[2]]) ) {
-                    return $inst->config[$key[0]][$key[1]][$key[2]];
-                } else {
-                    throw new Exception('Missing configuration: ' . $key[0] . '.' . $key[1] . '.' . $key[2]);
-                }
-                break;
+        foreach($config as $value) {
+            if( empty($obj[$value]) ) {
+                throw new Exception('Invalid config key: ' . $key);
+            }
+            $obj = $obj[$value];
         }
+        return $obj;
     }
 
     public function __clone() {
